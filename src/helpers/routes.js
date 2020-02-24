836794bb2e0569a7eefcaaa0e8a210129e63c9d4
m7rcin <merlin@magictm.com>
2020-02-24 11:18:34 +0100

    Update

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,23 +1,31 @@
-const { STRATEGIES } = require('./constants')
-const { extractComponentOptions } = require('./components')
-const { getPageOptions, getLocaleCodes } = require('./utils')
+const { STRATEGIES } = require("./constants")
+const { extractComponentOptions } = require("./components")
+const { getPageOptions, getLocaleCodes } = require("./utils")
 
-exports.makeRoutes = (baseRoutes, {
-  locales,
-  defaultLocale,
-  routesNameSeparator,
-  defaultLocaleRouteNameSuffix,
-  strategy,
-  parsePages,
-  pages,
-  encodePaths,
-  pagesDir,
-  differentDomains
-}) => {
+exports.makeRoutes = (
+  baseRoutes,
+  {
+    locales,
+    defaultLocale,
+    routesNameSeparator,
+    defaultLocaleRouteNameSuffix,
+    strategy,
+    parsePages,
+    pages,
+    encodePaths,
+    pagesDir,
+    differentDomains,
+  },
+) => {
   locales = getLocaleCodes(locales)
   let localizedRoutes = []
 
-  const buildLocalizedRoutes = (route, routeOptions = {}, isChild = false, isExtraRouteTree = false) => {
+  const buildLocalizedRoutes = (
+    route,
+    routeOptions = {},
+    isChild = false,
+    isExtraRouteTree = false,
+  ) => {
     const routes = []
     let pageOptions
 
@@ -30,7 +38,13 @@ exports.makeRoutes = (baseRoutes, {
     if (parsePages) {
       pageOptions = extractComponentOptions(route.component)
     } else {
-      pageOptions = getPageOptions(route, pages, locales, pagesDir, defaultLocale)
+      pageOptions = getPageOptions(
+        route,
+        pages,
+        locales,
+        pagesDir,
+        defaultLocale,
+      )
     }
 
     // Skip route if i18n is disabled on page
@@ -38,23 +52,42 @@ exports.makeRoutes = (baseRoutes, {
       return route
     }
 
+    // Disabled pages
+    const disabledPages = ["id-story"]
+
+    // Skip if page shouldn't be multilangual
+    for (const page of disabledPages) {
+      if (page === route.name) {
+        return route
+      }
+    }
+
     // Component's specific options
     const componentOptions = {
       locales,
       ...pageOptions,
-      ...routeOptions
+      ...routeOptions,
     }
     // Double check locales to remove any locales not found in pageOptions
     // This is there to prevent children routes being localized even though
     // they are disabled in the configuration
     if (
-      typeof componentOptions.locales !== 'undefined' && componentOptions.locales.length > 0 &&
-      typeof pageOptions.locales !== 'undefined' && pageOptions.locales.length > 0) {
-      componentOptions.locales = componentOptions.locales.filter((locale) => pageOptions.locales.includes(locale))
+      typeof componentOptions.locales !== "undefined" &&
+      componentOptions.locales.length > 0 &&
+      typeof pageOptions.locales !== "undefined" &&
+      pageOptions.locales.length > 0
+    ) {
+      componentOptions.locales = componentOptions.locales.filter((locale) =>
+        pageOptions.locales.includes(locale),
+      )
     }
 
     // Generate routes for component's supported locales
-    for (let i = 0, length1 = componentOptions.locales.length; i < length1; i++) {
+    for (
+      let i = 0, length1 = componentOptions.locales.length;
+      i < length1;
+      i++
+    ) {
       const locale = componentOptions.locales[i]
       const { name } = route
       let { path } = route
@@ -69,24 +102,39 @@ exports.makeRoutes = (baseRoutes, {
       if (route.children) {
         localizedRoute.children = []
         for (let i = 0, length1 = route.children.length; i < length1; i++) {
-          localizedRoute.children = localizedRoute.children.concat(buildLocalizedRoutes(route.children[i], { locales: [locale] }, true, isExtraRouteTree))
+          localizedRoute.children = localizedRoute.children.concat(
+            buildLocalizedRoutes(
+              route.children[i],
+              { locales: [locale] },
+              true,
+              isExtraRouteTree,
+            ),
+          )
         }
       }
 
       // Get custom path if any
       if (componentOptions.paths && componentOptions.paths[locale]) {
-        path = encodePaths ? encodeURI(componentOptions.paths[locale]) : componentOptions.paths[locale]
+        path = encodePaths
+          ? encodeURI(componentOptions.paths[locale])
+          : componentOptions.paths[locale]
       }
 
       // For PREFIX_AND_DEFAULT strategy and default locale:
       // - if it's a parent route, add it with default locale suffix added (no suffix if route has children)
       // - if it's a child route of that extra parent route, append default suffix to it
-      if (locale === defaultLocale && strategy === STRATEGIES.PREFIX_AND_DEFAULT) {
+      if (
+        locale === defaultLocale &&
+        strategy === STRATEGIES.PREFIX_AND_DEFAULT
+      ) {
         if (!isChild) {
           const defaultRoute = { ...localizedRoute, path }
 
           if (name) {
-            defaultRoute.name = localizedRoute.name + routesNameSeparator + defaultLocaleRouteNameSuffix
+            defaultRoute.name =
+              localizedRoute.name +
+              routesNameSeparator +
+              defaultLocaleRouteNameSuffix
           }
 
           if (defaultRoute.children) {
@@ -94,27 +142,37 @@ exports.makeRoutes = (baseRoutes, {
             defaultRoute.children = []
             for (const childRoute of route.children) {
               // isExtraRouteTree argument is true to indicate that this is extra route added for PREFIX_AND_DEFAULT strategy
-              defaultRoute.children = defaultRoute.children.concat(buildLocalizedRoutes(childRoute, { locales: [locale] }, true, true))
+              defaultRoute.children = defaultRoute.children.concat(
+                buildLocalizedRoutes(
+                  childRoute,
+                  { locales: [locale] },
+                  true,
+                  true,
+                ),
+              )
             }
           }
 
           routes.push(defaultRoute)
         } else if (isChild && isExtraRouteTree && name) {
-          localizedRoute.name += routesNameSeparator + defaultLocaleRouteNameSuffix
+          localizedRoute.name +=
+            routesNameSeparator + defaultLocaleRouteNameSuffix
         }
       }
 
-      const isChildWithRelativePath = isChild && !path.startsWith('/')
+      const isChildWithRelativePath = isChild && !path.startsWith("/")
 
       // Add route prefix if needed
-      const shouldAddPrefix = (
+      const shouldAddPrefix =
         // No prefix if app uses different locale domains
         !differentDomains &&
         // No need to add prefix if child's path is relative
         !isChildWithRelativePath &&
         // Skip default locale if strategy is PREFIX_EXCEPT_DEFAULT
-        !(locale === defaultLocale && strategy === STRATEGIES.PREFIX_EXCEPT_DEFAULT)
-      )
+        !(
+          locale === defaultLocale &&
+          strategy === STRATEGIES.PREFIX_EXCEPT_DEFAULT
+        )
 
       if (shouldAddPrefix) {
         path = `/${locale}${path}`
@@ -130,11 +188,13 @@ exports.makeRoutes = (baseRoutes, {
 
   for (let i = 0, length1 = baseRoutes.length; i < length1; i++) {
     const route = baseRoutes[i]
-    localizedRoutes = localizedRoutes.concat(buildLocalizedRoutes(route, { locales }))
+    localizedRoutes = localizedRoutes.concat(
+      buildLocalizedRoutes(route, { locales }),
+    )
   }
 
   try {
-    const { sortRoutes } = require('@nuxt/utils')
+    const { sortRoutes } = require("@nuxt/utils")
     localizedRoutes = sortRoutes(localizedRoutes)
   } catch (error) {
     // Ignore
--- types/vue.d.ts
+++ types/vue.d.ts
@@ -1,25 +1,26 @@
-import Vue from 'vue'
-import { RawLocation, Route } from 'vue-router'
-import VueI18n, { IVueI18n } from 'vue-i18n'
-import { NuxtI18nComponentOptions, NuxtVueI18n, NuxtI18nSeo } from './nuxt-i18n'
+import Vue from "vue"
+import { RawLocation, Route } from "vue-router"
+import VueI18n, { IVueI18n } from "vue-i18n"
+import { MetaInfo } from "vue-meta"
+import { NuxtI18nComponentOptions, NuxtVueI18n, NuxtI18nSeo } from "./nuxt-i18n"
 
 /**
  * Extends types in vue-i18n
  */
-declare module 'vue-i18n' {
+declare module "vue-i18n" {
   // the VueI18n class expands here: https://goo.gl/Xtp9EG
   // it is necessary for the $i18n property in Vue interface: "readonly $i18n: VueI18n & IVueI18n"
   interface IVueI18n extends NuxtVueI18n.Options.NuxtI18nInterface {
-    getLocaleCookie() : string | undefined
-    setLocaleCookie(locale: string) : undefined
-    setLocale(locale: string) : Promise<undefined>
+    getLocaleCookie(): string | undefined
+    setLocaleCookie(locale: string): undefined
+    setLocale(locale: string): Promise<undefined>
   }
 }
 
 /**
  * Extends types in vue
  */
-declare module 'vue/types/vue' {
+declare module "vue/types/vue" {
   interface Vue {
     localePath(route: RawLocation, locale?: string): string
     switchLocalePath(locale: string): string
@@ -30,7 +31,7 @@ declare module 'vue/types/vue' {
   }
 }
 
-declare module 'vue/types/options' {
+declare module "vue/types/options" {
   interface ComponentOptions<V extends Vue> {
     nuxtI18n?: NuxtI18nComponentOptions | false
   }
@@ -39,13 +40,13 @@ declare module 'vue/types/options' {
 /**
  * Extends types in Nuxt
  */
-declare module '@nuxt/types' {
+declare module "@nuxt/types" {
   interface NuxtAppOptions extends NuxtVueI18n.Options.NuxtI18nInterface {
     readonly i18n: VueI18n & IVueI18n
   }
 }
 
-declare module 'vuex/types/index' {
+declare module "vuex/types/index" {
   interface Store<S> {
     readonly $i18n: VueI18n & IVueI18n
   }
